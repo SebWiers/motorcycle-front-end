c95bf4dcd875759fe24dd7fa5e061bc15c82e7c3
Sebastian Wiers <sebwiers@gmail.com>
2018-05-06 17:49:36 -0500

    tweked table staleness

--- sandbox/do.js
+++ sandbox/do.js
@@ -1,20 +1,14 @@
 $('div#data table td input[type="number"]').change( function(){
     input.getNumber(this.id)
-
 })
 $('div#data table td input#COLOR').change( function(){
     input.getRangeColor() 
-
 })
 $('div#data table td input[type=radio][name=units]').change( function(){
-    $('canvas#bikeCanvas').addClass('stale')
-    $('div#charts').addClass('stale')
     input.convert(this.value)
 })
 $('#drawButton').click( function(){
     input.bike = input.makeBike()
     charts.draw(input.bike)
-    $('div#charts').removeClass('stale')
-    $('canvas#bikeCanvas').removeClass('stale')
 })
 $('#drawButton').click()
